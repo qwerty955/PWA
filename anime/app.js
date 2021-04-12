e0797305e14e4ac76e8b5bf788ab03a4c1672f00
qwerty955 <75160615+qwerty955@users.noreply.github.com>
2021-04-12 16:35:20 +0600

    Delete anime directory

--- anime/app.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js")
-    .then(()=>{
-        console.log('truuuuuuuuuuuuuuuuueee!!!!!!!!!!!!!!!!!!!!!');
-    })
-    .catch(()=>{
-        console.log('eeeerrrrooor');
-    });
-};
